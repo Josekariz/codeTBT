2a917ba7087fcf22f9912173ca78c43d4c13d7f2
Joseph Kariuki Macharia <sejokarizz@gmail.com>
2023-09-28 08:08:09 +0000

    create migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Author.create(name: "J.K. Rowling", bio: "British author best known for the Harry Potter series")
+Author.create(name: "George Orwell", bio: "English novelist and essayist, known for 1984 and Animal Farm")
+Author.create(name: "Jane Austen", bio: "English novelist known for Pride and Prejudice and Sense and Sensibility")
+
+# db/seeds/books.rb
+Book.create(title: "Harry Potter and the Sorcerer's Stone", pages: 320, author_id: 1, genre: "Fantasy")
+Book.create(title: "1984", pages: 328, author_id: 2, genre: "Dystopian Fiction")
+Book.create(title: "Pride and Prejudice", pages: 279, author_id: 3, genre: "Romantic Fiction")
